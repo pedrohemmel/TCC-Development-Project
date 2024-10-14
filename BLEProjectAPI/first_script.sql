e499a64e8d2e32dc5549043e260a8b7a522ed3d2
pedrohemmel <phenrique_h2016@hotmail.com>
2024-10-14 20:05:07 -0300

    feat: Start in creation of database scripts

---
+++ BLEProjectAPI/first_script.sql
@@ -0,0 +1,9 @@
+CREATE TABLE devices (
+    id SERIAL PRIMARY KEY,
+    local_name VARCHAR(255) NOT NULL,
+    device_id VARCHAR(255) NOT NULL,
+    timestamp TIMESTAMP NOT NULL
+);
+
+-- Adiciona um índice opcional para acelerar buscas por device_id
+CREATE INDEX idx_device_id ON devices (device_id);
